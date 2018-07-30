d6a6838ac96320584bc5b1fc954fc55e102c9b81
Kristen Papke <klpapke215@gmail.com>
2018-07-30 16:14:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,16 @@ class Song
     genre_count
   end
 
-  def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  def Song.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count += 1
+    else
+      artist_count[artist] = 1
+    end
   end
+  artist_count
+end
+
 end 
